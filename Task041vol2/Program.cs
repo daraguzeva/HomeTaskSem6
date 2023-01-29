ecb7de5bdec4596829b5ce335b94dcd9d43a5a8c
Daria <lara-lorn@yandex.ru>
2023-01-29 21:55:18 +0400

    save vol2 task 041

--- Task041vol2/Program.cs
+++ Task041vol2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите числа через запятую: ");
+string arr = Console.ReadLine();
+// Console.Write(arr);
+string[] num = arr.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+int[] n = num.Select(Int32.Parse).ToArray();
+//Console.WriteLine();
+// for (int i = 0; i < n.Length; i++)
+// {
+//     Console.Write($"{n[i]} ");
+// }
+int count = 0;
+for (int j = 0; j < n.Length; j++)
+{
+    if (n[j] > 0)
+    {
+        count++;
+    }
+}
+//Console.WriteLine();
+Console.Write($"Колличество положительных элементов в массиве: {count}.");
